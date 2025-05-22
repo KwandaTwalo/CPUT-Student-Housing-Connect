e72bb55659b50ee783b31f6a625c9a413e14532d
TylorTazz2024 <165163298+TylorTazz2024@users.noreply.github.com>
2025-05-23 01:15:27 +0200

    Updated

--- src/main/java/co/za/cput/domain/generic/Contact.java
+++ src/main/java/co/za/cput/domain/generic/Contact.java
@@ -1,5 +1,70 @@
 package co.za.cput.domain.generic;
 
+/* Contact.java
+   Domain class for Contact
+   Author: T.Malifethe 222602511
+*/
 public class Contact {
+    private String contactid;
+    private String email;
+    private String phoneNumber;
 
+    private Contact(Builder builder) {
+        this.contactid = builder.contactid;
+        this.email = builder.email;
+        this.phoneNumber = builder.phoneNumber;
+    }
+
+    public String getContactid() {
+        return contactid;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "contactid='" + contactid + '\'' +
+                ", email='" + email + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+        private String contactid;
+        private String email;
+        private String phoneNumber;
+
+        public Builder setContactid(String contactid) {
+            this.contactid = contactid;
+            return this;
+        }
+
+        public Builder setEmail(String email) {
+            this.email = email;
+            return this;
+        }
+
+        public Builder setPhoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder copy(Contact contact) {
+            this.contactid = contact.contactid;
+            this.email = contact.email;
+            this.phoneNumber = contact.phoneNumber;
+            return this;
+        }
+
+        public Contact build() {
+            return new Contact(this);
+        }
+    }
 }
--- src/main/java/co/za/cput/factory/generic/ContactFactory.java
+++ src/main/java/co/za/cput/factory/generic/ContactFactory.java
@@ -1,4 +1,25 @@
 package co.za.cput.factory.generic;
 
+import co.za.cput.domain.generic.Contact;
+import co.za.cput.util.Helper;
+
+import java.util.UUID;
+
 public class ContactFactory {
+
+    public static Contact createContact(String email, String phoneNumber) {
+
+        String contactId = UUID.randomUUID().toString();
+
+        if (!Helper.validateStringDetails(email) || !Helper.validateStringDetails(phoneNumber)) {
+
+            return null;
+        }
+
+        return new Contact.Builder()
+                .setContactid(contactId)
+                .setEmail(email)
+                .setPhoneNumber(phoneNumber)
+                .build();
+    }
 }
