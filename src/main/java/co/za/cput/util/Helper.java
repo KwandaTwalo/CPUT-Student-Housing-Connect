6767fee03d277c2752f5f1297c9adfb0c799daef
Agnes-99 <Agnesmabusela8@gmail.com>
2025-05-23 20:26:05 +0200

    Updated Helper.java

--- src/main/java/co/za/cput/util/Helper.java
+++ src/main/java/co/za/cput/util/Helper.java
@@ -3,6 +3,8 @@ package co.za.cput.util;
 import java.time.LocalDate;
 import java.util.Date;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Helper {
 
@@ -63,6 +65,38 @@ public class Helper {
     //Sinhle
 
     //Agnes
+    public static boolean isValidPhoneNumber(String phoneNumber) {
+        if (validateStringDetails(phoneNumber)) {
+            return false;
+        }
+
+        //Checks for e.g 0725637252 or +27 72 563...
+        String regex = "^(\\+27|0)[6-8][0-9]{8}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+
+    public static boolean isValidEmail(String email) {
+        if (validateStringDetails(email)) {
+            return false;
+        }
+        String regex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+
+    public static boolean isValidPassword(String password) {
+        if (validateStringDetails(password)) {
+            return false;
+        }
+        //Checks if Password has at least one digit, one letter and is more than 8 characters long and No Special Characters.
+        String regex = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]{8,}$";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(password);
+        return matcher.matches();
+    }
 
     //Tandile
 
