b6d9a78d0a25dcf11cc9da72bb0d585360354344
Agnes-99 <Agnesmabusela8@gmail.com>
2025-05-23 20:25:44 +0200

    Added AdministratorFactory.java

--- src/main/java/co/za/cput/factory/user/AdministratorFactory.java
+++ src/main/java/co/za/cput/factory/user/AdministratorFactory.java
@@ -1,4 +1,34 @@
 package co.za.cput.factory.user;
 
+import co.za.cput.domain.users.Administrator;
+import co.za.cput.util.Helper;
+
 public class AdministratorFactory {
+
+    public static Administrator createAdministrator(Integer id, String name, String surname, String email, String phoneNumber, String password) {
+
+        if(Helper.validateStringDetails(name)|| Helper.validateStringDetails(surname)|| Helper.validateStringDetails(email)){
+            return null;
+        }
+
+        if(!(Helper.isValidEmail(email))){
+            return null;
+        }
+
+        if(!(Helper.isValidPhoneNumber(phoneNumber))){
+            return null;
+        }
+
+        if(!(Helper.isValidPassword(password))){
+            return null;
+        }
+
+        return new Administrator.Builder()
+                .setName(name)
+                .setSurname(surname)
+                .setEmail(email)
+                .setPhoneNumber(phoneNumber)
+                .setPassword(password)
+                .build();
+    }
 }
