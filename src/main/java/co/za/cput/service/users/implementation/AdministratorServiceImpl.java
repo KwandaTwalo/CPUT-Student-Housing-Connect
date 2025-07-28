652321bfc43c0f2a71a4c9b47bc6f2b66660ed19
Agnes-99 <Agnesmabusela8@gmail.com>
2025-07-28 14:51:22 +0200

    Updated AdministratorServiceImpl.java

--- src/main/java/co/za/cput/service/users/implementation/AdministratorServiceImpl.java
+++ src/main/java/co/za/cput/service/users/implementation/AdministratorServiceImpl.java
@@ -1,4 +1,44 @@
 package co.za.cput.service.users.implementation;
 
-public class AdministratorServiceImpl {
+import co.za.cput.domain.users.Administrator;
+import co.za.cput.repository.users.AdministratorRepository;
+import co.za.cput.service.users.IAdministratorService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AdministratorServiceImpl implements IAdministratorService {
+
+    @Autowired
+    private static IAdministratorService service;
+    private AdministratorRepository repository;
+
+    @Override
+    public Administrator create(Administrator administrator) {
+        return this.repository.save(administrator);
+    }
+
+    @Override
+    public Administrator read(Long id) {
+        return this.repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Administrator update(Administrator administrator) {
+        return this.repository.save(administrator);
+    }
+
+    @Override
+    public boolean delete(Long id) {
+        this.repository.deleteById(id);
+        return true;
+    }
+
+    @Override
+    public List<Administrator> getAll() {
+        return this.repository.findAll();
+    }
 }
