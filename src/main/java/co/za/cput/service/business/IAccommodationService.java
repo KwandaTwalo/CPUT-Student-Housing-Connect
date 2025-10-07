ee0f42d1ff8964fa17357f3245e57dd9ce11b6aa
Ethan <ehector.eho100@gmail.com>
2025-10-07 16:09:36 +0200

    Project Finish Fix

--- src/main/java/co/za/cput/service/business/IAccommodationService.java
+++ src/main/java/co/za/cput/service/business/IAccommodationService.java
@@ -4,10 +4,13 @@ package co.za.cput.service.business;
 //Student Number:   221802797.
 
 import co.za.cput.domain.business.Accommodation;
+import co.za.cput.dto.AccommodationSearchCriteria;
+import co.za.cput.dto.AccommodationSummary;
 import co.za.cput.service.IService;
 
 import java.util.List;
 
 public interface IAccommodationService extends IService<Accommodation, Long> {
     List<Accommodation> getAllAccommodations();
+    List<AccommodationSummary> search(AccommodationSearchCriteria criteria);
 }
